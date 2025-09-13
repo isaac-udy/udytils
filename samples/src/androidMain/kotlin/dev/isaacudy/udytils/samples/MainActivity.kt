15b7b7aa4ea48783c66eec0ca075cac119cfbc8e
Isaac Udy <isaac.udy@gmail.com>
2025-09-13 22:31:34 +1200

    update status bar appearance based on system theme in main activity

--- samples/src/androidMain/kotlin/dev/isaacudy/udytils/samples/MainActivity.kt
+++ samples/src/androidMain/kotlin/dev/isaacudy/udytils/samples/MainActivity.kt
@@ -4,28 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+
         setContent {
-            Scaffold { paddingValues ->
-                Box(
-                    modifier = Modifier
-                        .padding(paddingValues)
-                        .fillMaxSize()
-                ) {
-                    SamplesApplicationContent()
-                }
+            val isDarkTheme = isSystemInDarkTheme()
+            LaunchedEffect(isDarkTheme) {
+                WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = !isDarkTheme
             }
+            SamplesApplicationContent()
         }
     }
 }
