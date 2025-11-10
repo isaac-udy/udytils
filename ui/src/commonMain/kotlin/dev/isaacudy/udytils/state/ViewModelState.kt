3c7350b30d4ba7adc734698b8930a49f0a4e8e4e
Isaac Udy <isaac.udy@gmail.com>
2025-11-11 01:24:46 +1300

    Added functionality to build a ViewModelState based on a lamba, which allows flow observations that result in state updates to be kept private to the state creation

--- ui/src/commonMain/kotlin/dev/isaacudy/udytils/state/ViewModelState.kt
+++ ui/src/commonMain/kotlin/dev/isaacudy/udytils/state/ViewModelState.kt
@@ -30,6 +30,53 @@ class ViewModelState<T>(
     fun collectAsState(): State<T> {
         return stateFlow.collectAsState()
     }
+
+    companion object {
+        context(viewModel: ViewModel)
+        fun <T> build(
+            block: Builder<T>.() -> T
+        ) : ViewModelState<T> {
+            val builder = Builder<T>(viewModel)
+            return ViewModelState(
+                MutableStateFlow(block(builder))
+            ).also { state ->
+                builder.bind(state)
+            }
+        }
+    }
+
+    class Builder<T> internal constructor(
+        private val viewModel: ViewModel,
+    ) {
+        private var boundState: ViewModelState<T>? = null
+        private val queuedUpdates: MutableList<T.() -> T> = mutableListOf()
+        val state: StateReference = StateReference()
+
+        internal fun bind(state: ViewModelState<T>) {
+            require(this.boundState == null)
+            this.boundState = state
+            val queued = queuedUpdates.toList()
+            queuedUpdates.clear()
+            viewModel.run {
+                queued.forEach { queuedUpdate ->
+                    state.update(queuedUpdate)
+                }
+            }
+        }
+
+        inner class StateReference {
+            fun update(block: T.() -> T) {
+                val current = boundState
+                if (current == null) {
+                    queuedUpdates.plus(block)
+                    return
+                }
+                viewModel.run {
+                    current.update { block(this) }
+                }
+            }
+        }
+    }
 }
 
 context(viewModel: ViewModel)
@@ -38,4 +85,3 @@ fun <T> viewModelState(
 ) : ViewModelState<T> {
     return ViewModelState(MutableStateFlow(initialState))
 }
-
