317316f00b69f7bd6ea62d277ddadc6cd58c9114
Isaac Udy <isaac.udy@gmail.com>
2025-12-17 10:39:47 +1300

    Add TODOs for WASM/JS permissions

---
+++ ui/src/jvmMain/kotlin/dev/isaacudy/udytils/permissions/Permission.jvm.kt
@@ -0,0 +1,5 @@
+package dev.isaacudy.udytils.permissions
+
+actual fun hasPermission(permission: Permission): Boolean {
+    TODO("Not yet implemented")
+}
---
+++ ui/src/wasmJsMain/kotlin/dev/isaacudy/udytils/permissions/Permission.wasmJs.kt
@@ -0,0 +1,5 @@
+package dev.isaacudy.udytils.permissions
+
+actual fun hasPermission(permission: dev.isaacudy.udytils.permissions.Permission): Boolean {
+    TODO("Not yet implemented")
+}
