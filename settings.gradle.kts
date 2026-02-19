443e8f1101b2e84bc38074e71ee72e0791268adc
Isaac Udy <isaac.udy@gmail.com>
2026-02-20 01:46:38 +1300

    Remove enro included build from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,19 +18,3 @@ dependencyResolutionManagement {
 include(":core")
 include(":samples")
 include(":ui")
-
-includeBuild("../embedded-enro") {
-    name = "embedded-enro"
-    dependencySubstitution {
-        substitute(module("dev.enro:enro-processor")).using(project(":enro-processor"))
-        substitute(module("dev.enro:enro-annotations")).using(project(":enro-annotations"))
-        substitute(module("dev.enro:enro-test")).using(project(":enro-test"))
-        substitute(module("dev.enro:enro-lint")).using(project(":enro-lint"))
-        substitute(module("dev.enro:enro")).using(project(":enro"))
-        substitute(module("dev.enro:enro-common")).using(project(":enro-common"))
-        substitute(module("dev.enro:enro-runtime")).using(project(":enro-runtime"))
-        substitute(module("dev.enro:enro-compat")).using(project(":enro-compat"))
-        substitute(module("dev.enro:tests:application")).using(project(":tests:application"))
-        substitute(module("dev.enro:tests:module-one")).using(project(":tests:module-one"))
-    }
-}
